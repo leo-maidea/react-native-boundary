1b2acc0a31c438107683b3bde5bd1efd34847063
Leo Kolar <leo.kolar@maidea.com>
2021-01-26 18:28:57 +0100

    android 10 quick-fix

--- android/src/main/java/com/eddieowens/RNBoundaryModule.java
+++ android/src/main/java/com/eddieowens/RNBoundaryModule.java
@@ -4,8 +4,8 @@ import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import android.util.Log;
 
 import com.eddieowens.receivers.BoundaryEventBroadcastReceiver;
